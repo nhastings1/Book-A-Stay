88434a546b17e223892d003ddfb87e5833821087
Nick Hastings <nickhastings@Clients-MacBook-Pro-2.local>
2022-12-06 18:24:52 -0600

    push src/context structure

--- book-a-stay-dev/src/index.js
+++ book-a-stay-dev/src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+import App from "./App/App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// import react-router-dom
+import { BrowserRouter as Router } from "react-router-dom";
+
+// import context
+import { RoomProvider } from "./Context/Context";
+
+ReactDOM.render(
+  <RoomProvider>
+    <Router>
+      <App />
+    </Router>
+  </RoomProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
